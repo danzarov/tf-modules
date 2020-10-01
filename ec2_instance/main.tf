3d855c514ff9562f24c1fbc1950c86ebc7ee2d6b
sometimesdanny <danzarov@gmail.com>
2020-10-01 15:55:55 -0300

    simplemod

---
+++ ec2_instance/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {}
+
+resource "aws_instance" "example" {
+  ami           = var.aws_ami
+  instance_type = var.aws_instance_type
+  key_name      = var.aws_key_name
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ ec2_instance/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_ami" {
+  description = "AWS AMI"
+}
+
+variable "aws_instance_type" {
+  description = "AWS INSTANCE TYPE"
+}
+
+variable "server_port" {
+  description = "instance port to allow access"
+}
+
+variable "aws_key_name" {
+  description = "AWS USER KEY NAME"
+}
